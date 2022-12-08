3eac00ea7f9fedbc5983273704e7b00e06cbb4a7
alarksharma <alarksharma@gmail.com>
2022-12-08 11:37:40 +0530

    Implementing passwordLength rule

--- src/main/java/org/password/verify/PasswordLength.java
+++ src/main/java/org/password/verify/PasswordLength.java
@@ -3,6 +3,12 @@ package org.password.verify;
 public class PasswordLength implements Rule{
     @Override
     public boolean isSatisfiedOn(String s) {
+        return isLengthGreaterThan8(s);
+    }
+
+    public boolean isLengthGreaterThan8(String s){
+        if(s.length()>8)
+            return true;
         return false;
     }
 }
