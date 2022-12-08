c293c476424f479d164fcf40807b920470fe0174
alarksharma <alarksharma@gmail.com>
2022-12-08 16:43:15 +0530

    Implementing the logic for feature 2 by making lower case condition mandatory

--- src/main/java/org/password/verify/PasswordValidator.java
+++ src/main/java/org/password/verify/PasswordValidator.java
@@ -2,27 +2,51 @@ package org.password.verify;
 
 import org.password.verify.exceptions.PasswordValidationException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public class PasswordValidator {
     private static final List<Rule> rules = asList(
-            new PasswordLowercaseRule(),
             new PasswordNumberRule(),
-            new PasswordUppercaseRule(),
             new PasswordNotNullRule(),
+            new PasswordUppercaseRule(),
+            new PasswordLowercaseRule(),
             new PasswordLengthRule());
 
     public boolean validate(String password) {
-        return rules.stream().allMatch(rule -> {
+        return rules.parallelStream().allMatch(rule -> {
             try {
                 return rule.isSatisfiedOn(password);
             } catch (PasswordValidationException e) {
-                throw new RuntimeException(e);
             }
+            return false;
         });
     }
+
+    public boolean validateAny3(String password) throws PasswordValidationException {
+        List<Boolean> result=new ArrayList<Boolean>();
+         rules.parallelStream().forEach(rule -> {
+            try {
+                 result.add(rule.isSatisfiedOn(password));
+            } catch (PasswordValidationException e) {
+            }
+        });
+        int count=0;
+        for (boolean b:result) {
+            if(b)
+                count++;
+        }
+        if(count>=3)
+            return true;
+        throw new PasswordValidationException("More Than 3 conditions should be true");
+    }
+
+    public boolean validateAny3AndLowercase(String password) throws PasswordValidationException {
+        return validateAny3(password) && new PasswordLowercaseRule().isSatisfiedOn(password);
+    }
+
 }
 
 
--- src/test/java/org/password/verify/PasswordValidatorTest.java
+++ src/test/java/org/password/verify/PasswordValidatorTest.java
@@ -1,12 +1,35 @@
 package org.password.verify;
 
 import org.junit.Test;
+import org.password.verify.exceptions.PasswordValidationException;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PasswordValidatorTest {
     @Test
-    public void test(){
+    public void validateCorrectPassword(){
         assertTrue(new PasswordValidator().validate("123456789aA"));
+    }
+    @Test
+    public void validateIncorrectPassword(){
+        assertFalse(new PasswordValidator().validate("12"));
+    }
+    @Test
+    public void validateAny3Condition_true() throws PasswordValidationException {
+        assertTrue(new PasswordValidator().validateAny3("1234aA"));
+    }
+    @Test(expected = PasswordValidationException.class)
+    public void validateAny3Condition_false() throws PasswordValidationException {
+        assertTrue(new PasswordValidator().validateAny3("12"));
+    }
+
+    @Test
+    public void validateAny3andLowercaseCondition_true() throws PasswordValidationException {
+        assertTrue(new PasswordValidator().validateAny3AndLowercase("123456789a"));
+    }
+    @Test(expected = PasswordValidationException.class)
+    public void validateAny3andLowercaseCondition_false() throws PasswordValidationException {
+        assertTrue(new PasswordValidator().validateAny3AndLowercase("123456789A"));
     }
 }
