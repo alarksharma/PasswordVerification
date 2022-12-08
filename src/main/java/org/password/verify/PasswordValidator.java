154e0a6f17b7dbc2a4ffcf7381a4c3b49db25cc3
alarksharma <alarksharma@gmail.com>
2022-12-08 14:33:18 +0530

    Implementing password validator Logic

--- src/main/java/org/password/verify/PasswordValidator.java
+++ src/main/java/org/password/verify/PasswordValidator.java
@@ -1,4 +1,28 @@
 package org.password.verify;
 
+import org.password.verify.exceptions.PasswordValidationException;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class PasswordValidator {
+    private static final List<Rule> rules = asList(
+            new PasswordLowercaseRule(),
+            new PasswordNumberRule(),
+            new PasswordUppercaseRule(),
+            new PasswordNotNullRule(),
+            new PasswordLengthRule());
+
+    public boolean validate(String password) {
+        return rules.stream().allMatch(rule -> {
+            try {
+                return rule.isSatisfiedOn(password);
+            } catch (PasswordValidationException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
+
+
--- src/test/java/org/password/verify/PasswordValidatorTest.java
+++ src/test/java/org/password/verify/PasswordValidatorTest.java
@@ -1,4 +1,12 @@
 package org.password.verify;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class PasswordValidatorTest {
+    @Test
+    public void test(){
+        assertTrue(new PasswordValidator().validate("123456789aA"));
+    }
 }
